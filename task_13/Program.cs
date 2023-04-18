82dea1aef0ba4fcbc5bc757af96e964485629176
Dmitriy <dmitrypolyakovd@gmail.com>
2023-04-18 04:36:42 +0300

    third

---
+++ task_13/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Вывод третьей цифры заданного числа.
+645   -> 5
+78    -> третьей цифры нет
+32679 -> 6
+*/
+
+
+Console.Write("Введите любое число: ");
+string s = Console.ReadLine();
+
+if (int.TryParse(s, out int c))
+{
+    if (c >= 100 && c < 1000)
+    {
+        Console.WriteLine($"Третья цифра числа {c}: {(c / 1) % 10}");
+    }
+    else if (c < 100){
+        Console.WriteLine($"Третья цифра числа {c} отсутствует");
+    }
+    else if (c >= 1000 && c < 10000)
+    {
+        Console.WriteLine($"Третья цифра числа {c}: {(c / 10) % 10}");
+    }
+    else if (c >= 10000 && c < 100000)
+    {
+        Console.WriteLine($"Третья цифра числа {c}: {(c / 100) % 10}");
+    }
+    else if (c >= 100000)
+    {
+        Console.WriteLine($"Слишком больше число {c}");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка при вводе числа");
+}
