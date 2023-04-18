ecf705b7a80b12669e36000c9044e55c84b0a221
Dmitriy <dmitrypolyakovd@gmail.com>
2023-04-18 04:56:47 +0300

    день недели

---
+++ task_15/Program.cs
@@ -0,0 +1,27 @@
+﻿// День недели.
+
+Console.Write("Введите день недели: ");
+if (int.TryParse(Console.ReadLine(), out int number))
+{
+    switch (number)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            Console.WriteLine("День недели не является выходным");
+            break;
+        case 6:
+        case 7:
+            Console.WriteLine("День недели является выходным");
+            break;
+        default:
+            Console.WriteLine("Вы ввели некорректный день недели.");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не число.");
+}
